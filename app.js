66300e4f088fd62af25a2e94b875bb4d5935aea0
juggler39 <juggler39@yandex.ru>
2020-05-28 20:26:37 +0200

    add class Field

--- app.js
+++ app.js
@@ -4,29 +4,21 @@ window.addEventListener('DOMContentLoaded', () => {
     const ROWS = 20;
     const FIELD_SIZE = COLS*ROWS;
 
-    //field initialisation
-    for (let i = 0; i<FIELD_SIZE; i++) {
-        document.getElementById('field').appendChild(document.createElement('div'));
-    }
-
     class Field {
-        grid;
 
-        // Reset the field when we start a new game.
-        reset() {
-            this.grid = this.getEmptyBoard();
+        constructor() {
+            for (let i = 0; i<FIELD_SIZE; i++) {
+                document.getElementById('field').appendChild(document.createElement('div'));
+            }
         }
-
-        // Get matrix filled with zeros.
-        getEmptyBoard() {
-            return Array.from(
-                {length: ROWS}, () => Array(COLS).fill(0)
-            );
+        clearField() {
+            return Array.from({length: ROWS}, () => Array(COLS).fill(0));
         }
     }
 
 
     let field = new Field();
+    field.clearField();
 
     function play() {
         field.reset();
