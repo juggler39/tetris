f886a53ca017facd1c9e9efba629aace5354b8af
juggler39 <juggler39@yandex.ru>
2020-06-16 11:32:57 +0200

    almost playable game

--- app.js
+++ app.js
@@ -1,56 +1,58 @@
-const COLS = 10;
-const ROWS = 20;
-const COLORS=['cyan', 'yellow', 'blue', 'orange',  'green', 'purple', 'red'];
-const TETROMINO = [//I, O, J, L, S, T, Z - tetromino shapes
+const GAMEDATA = {
+cols: 10,
+rows: 20,
+colors: ['cyan', 'yellow', 'blue', 'orange', 'green', 'purple', 'red'],     //Standard tetromino colors
+tetromino: [//I, O, J, L, S, T, Z - tetromino shapes
     [[0, 0, 0, 0],
      [1, 1, 1, 1],
      [0, 0, 0, 0],
      [0, 0, 0, 0]],
 
-    [[1, 1],
-     [1, 1]],
+    [[2, 2],
+     [2, 2]],
 
-    [[1, 0, 0],
-     [1, 1, 1],
+    [[3, 0, 0],
+     [3, 3, 3],
      [0, 0, 0]],
 
-    [[0, 0, 1],
-     [1, 1, 1],
+    [[0, 0, 4],
+     [4, 4, 4],
      [0, 0, 0]],
 
-    [[0, 1, 1],
-     [1, 1, 0],
+    [[0, 5, 5],
+     [5, 5, 0],
      [0, 0, 0]],
 
-    [[0, 1, 0],
-     [1, 1, 1],
+    [[0, 6, 0],
+     [6, 6, 6],
      [0, 0, 0]],
 
-    [[1, 1, 0],
-     [0, 1, 1],
+    [[7, 7, 0],
+     [0, 7, 7],
      [0, 0, 0]]
 
-];
+],
 
-const kickData =[
-        [[0, 0], [-1, 0], [-1, 1], [ 0,-2], [-1,-2]],
-        [[0, 0], [ 1, 0], [ 1,-1], [ 0, 2], [ 1, 2]],
-        [[0, 0], [ 1, 0], [ 1, 1], [ 0,-2], [ 1,-2]],
-        [[0, 0], [-1, 0], [-1,-1], [ 0, 2], [-1, 2]],
-        [[0, 0], [ 1, 0], [ 1, 1], [ 0,-2], [ 1,-2]],
-        [[0, 0], [ 1, 0], [ 1,-1], [ 0, 2], [ 1, 2]],
-        [[0, 0], [-1, 0], [-1, 1], [ 0,-2], [-1,-2]],
-        [[0, 0], [-1, 0], [-1,-1], [ 0, 2], [-1, 2]]];
+kickData: [
+    [[0, 0], [-1, 0], [-1, 1], [0, -2], [-1, -2]],
+    [[0, 0], [1, 0], [1, -1], [0, 2], [1, 2]],
+    [[0, 0], [1, 0], [1, 1], [0, -2], [1, -2]],
+    [[0, 0], [-1, 0], [-1, -1], [0, 2], [-1, 2]],
+    [[0, 0], [1, 0], [1, 1], [0, -2], [1, -2]],
+    [[0, 0], [1, 0], [1, -1], [0, 2], [1, 2]],
+    [[0, 0], [-1, 0], [-1, 1], [0, -2], [-1, -2]],
+    [[0, 0], [-1, 0], [-1, -1], [0, 2], [-1, 2]]],
 
-const kickDataI =[
-        [[ 0, 0], [-2, 0], [ 1, 0], [-2,-1], [ 1, 2]],
-        [[ 0, 0], [-1, 0], [ 2, 0], [-1, 2], [ 2,-1]],
-        [[ 0, 0], [ 2, 0], [-1, 0], [ 2, 1], [-1,-2]],
-        [[ 0, 0], [ 1, 0], [-2, 0], [ 1,-2], [-2, 1]],
-        [[ 0, 0], [-1, 0], [ 2, 0], [-1, 2], [ 2,-1]],
-        [[ 0, 0], [ 2, 0], [-1, 0], [ 2, 1], [-1,-2]],
-        [[ 0, 0], [ 1, 0], [-2, 0], [ 1,-2], [-2, 1]],
-        [[ 0, 0], [-2, 0], [ 1, 0], [-2,-1], [ 1, 2]]];
+kickDataI: [
+    [[0, 0], [-2, 0], [1, 0], [-2, -1], [1, 2]],
+    [[0, 0], [-1, 0], [2, 0], [-1, 2], [2, -1]],
+    [[0, 0], [2, 0], [-1, 0], [2, 1], [-1, -2]],
+    [[0, 0], [1, 0], [-2, 0], [1, -2], [-2, 1]],
+    [[0, 0], [-1, 0], [2, 0], [-1, 2], [2, -1]],
+    [[0, 0], [2, 0], [-1, 0], [2, 1], [-1, -2]],
+    [[0, 0], [1, 0], [-2, 0], [1, -2], [-2, 1]],
+    [[0, 0], [-2, 0], [1, 0], [-2, -1], [1, 2]]],
+};
 
 class Field {
 
@@ -58,15 +60,15 @@ class Field {
     tetromino;
 
     constructor() {
-        for (let y = 0; y < ROWS; y++) {
-            for (let x = 0; x < COLS; x++) {
+        for (let y = 0; y < GAMEDATA.rows; y++) {
+            for (let x = 0; x < GAMEDATA.cols; x++) {
                 let cell = document.createElement('div');
                 cell.setAttribute("id", `x${x}y${y}`);
                 cell.setAttribute("class", `cell empty`);
                 document.getElementById('field').appendChild(cell);
             }
         }
-        this.arrField = Array.from({length: ROWS}, () => Array(COLS).fill(0));
+        this.arrField = Array.from({length: GAMEDATA.rows}, () => Array(GAMEDATA.cols).fill(0));
     }
 
     clearField() {
@@ -74,109 +76,43 @@ class Field {
         for (let i = 0; i<cells.length; i++) {
             cells[i].setAttribute ("class", `cell empty`);
         }
-        this.arrField = Array.from({length: ROWS}, () => Array(COLS).fill(0));
+        this.arrField = Array.from({length: GAMEDATA.rows}, () => Array(GAMEDATA.cols).fill(0));
+    }
 
-        document.getElementById(`x5y5`)
-            .setAttribute("class", `cell colorblue`);
-        this.arrField[5][5]=1;
+    repaint () {
+        for (let y = 0; y < GAMEDATA.rows; y++) {
+            for (let x = 0; x < GAMEDATA.cols; x++) {
+                let cell = document.getElementById(`x${x}y${y}`);
+                if (this.arrField[y][x]===0) {
+                    cell.setAttribute("class", `cell empty`);
+                } else {
+                    cell.setAttribute("class", `cell color${GAMEDATA.colors[this.arrField[y][x]-1]}`);
+                }
+            }
+        }
     }
 
     startGame() {
-       // this.clearField();
+        this.clearField();
         this.tetromino=new Tetromino();
-        this.tetromino.spawn();
-        this.tetromino.draw();
     }
 
-    moveDown () {
-        if (this.canMove (this.tetromino.shape, this.tetromino.x, this.tetromino.y+1)) {
-            this.tetromino.erase();
-            this.tetromino.y=this.tetromino.y+1;
-            this.tetromino.draw();
-        } else {
-            this.freeze ();
-        }
+    clearRows () {
+        this.arrField.forEach((row, y) => {
+             if (row.every(value => value > 0)) {
+                this.arrField.splice(y, 1);
+                this.arrField.unshift(Array(GAMEDATA.cols).fill(0));
+                this.repaint();
+            }
+        });
     }
+
     freeze () {
         for (let y = 0; y < this.tetromino.shape.length; y++) {
             for (let x = 0; x < this.tetromino.shape[y].length; x++) {
                 if (this.tetromino.shape[y][x]!==0) {
                     this.arrField[y+this.tetromino.y][x+this.tetromino.x]=this.tetromino.shape[y][x];
                 }
-            }
-        }
-
-    }
-
-    moveLeft () {
-        if (this.canMove (this.tetromino.shape, this.tetromino.x-1, this.tetromino.y)) {
-            this.tetromino.erase();
-            this.tetromino.x=this.tetromino.x-1;
-            this.tetromino.draw();
-        }
-    }
-
-    moveRight () {
-        if (this.canMove (this.tetromino.shape, this.tetromino.x+1, this.tetromino.y)) {
-            this.tetromino.erase();
-            this.tetromino.x=this.tetromino.x+1;
-            this.tetromino.draw();
-        }
-    }
-
-    rotateClockwise () {
-
-        let clone = JSON.parse(JSON.stringify(this.tetromino.shape)); //copying of bidimentional array
-        for (let y = 0; y < clone.length; y++) {
-            for (let x = 0; x < y; x++) {
-                [clone[x][y], clone[y][x]] = [clone[y][x], clone[x][y]];
-            }
-        }
-        clone.forEach(row => row.reverse());
-        for (let i=0;i<5;i++) {
-            let dx = kickData[this.tetromino.rotation][i][0];
-            let dy = kickData[this.tetromino.rotation][i][1];
-            if (this.tetromino.piece===0) {
-                dx = kickDataI[this.tetromino.rotation][i][0];
-                dy = kickDataI[this.tetromino.rotation][i][1];
-            }
-            if (this.canMove(clone, this.tetromino.x+dx, this.tetromino.y+dy)) {
-                this.tetromino.erase();
-                this.tetromino.shape = clone.slice();
-                this.tetromino.x+=dx;
-                this.tetromino.y+=dy;
-                this.tetromino.draw();
-                this.tetromino.rotation++;
-                if (this.tetromino.rotation === 4) this.tetromino.rotation = 0;
-                break;
-            }
-        }
-    }
-
-    rotateCounterClockwise () {
-        let clone = JSON.parse(JSON.stringify(this.tetromino.shape));
-        clone.forEach(row => row.reverse());
-        for (let y = 0; y < clone.length; y++) {
-            for (let x = 0; x < y; x++) {
-                [clone[x][y], clone[y][x]] = [clone[y][x], clone[x][y]];
-            }
-        }
-        for (let i=0;i<5;i++) {
-            let dx = kickData[this.tetromino.rotation+4][i][0];
-            let dy = kickData[this.tetromino.rotation+4][i][1];
-            if (this.tetromino.piece===0) {
-                dx = kickDataI[this.tetromino.rotation+4][i][0];
-                dy = kickDataI[this.tetromino.rotation+4][i][1];
-            }
-            if (this.canMove(clone, this.tetromino.x+dx, this.tetromino.y+dy)) {
-                this.tetromino.erase();
-                this.tetromino.shape = clone.slice();
-                this.tetromino.x+=dx;
-                this.tetromino.y+=dy;
-                this.tetromino.draw();
-                this.tetromino.rotation--;
-                if (this.tetromino.rotation === -1) this.tetromino.rotation = 3;
-                break;
             }
         }
     }
@@ -190,10 +126,10 @@ class Field {
         });
     };
     insideWalls(x) {
-        return x >= 0 && x < COLS;
+        return x >= 0 && x < GAMEDATA.cols;
     }
     aboveFloor(y) {
-        return y < ROWS;
+        return y < GAMEDATA.rows;
     }
     notOccupied(x, y) {
         return this.arrField[y] && this.arrField[y][x] === 0;
@@ -209,18 +145,19 @@ class Tetromino {
 
     constructor() {
         this.spawn();
+        this.draw();
         this.rotation=0;
     }
 
     spawn() {
         let randNumber = this.randomize();
         this.piece = randNumber;
-        this.shape = TETROMINO[randNumber];
+        this.shape = GAMEDATA.tetromino[randNumber];
         randNumber===1?this.x = 4:this.x = 3; //O-tetromino spawns in the center
         this.y = 0;
     }
     randomize() {
-         return Math.floor(Math.random() * (TETROMINO.length));
+         return Math.floor(Math.random() * (GAMEDATA.tetromino.length));
     }
 
     draw() {
@@ -228,7 +165,7 @@ class Tetromino {
             row.forEach((value, x) => {
                 if (value > 0) {
                     document.getElementById(`x${this.x + x}y${this.y + y}`)
-                        .setAttribute("class", `cell color${COLORS[this.piece]}`);
+                        .setAttribute("class", `cell color${GAMEDATA.colors[this.piece]}`);
                 }
             });
         });
@@ -239,48 +176,133 @@ class Tetromino {
             row.forEach((value, x) => {
                 if (value > 0) {
                     document.getElementById(`x${this.x + x}y${this.y + y}`)
-                        .setAttribute("class", `empty`);
+                        .setAttribute("class", `cell empty`);
                 }
             });
         });
     }
+
+    moveDown () {
+        if (field.canMove (this.shape, this.x, this.y+1)) {
+            this.erase();
+            this.y=this.y+1;
+            this.draw();
+        } else {
+            field.freeze ();
+            field.tetromino=new Tetromino();
+            field.clearRows ();
+        }
+    }
+
+    moveLeft () {
+        if (field.canMove (this.shape, this.x-1, this.y)) {
+            this.erase();
+            this.x=this.x-1;
+            this.draw();
+        }
+    }
+
+    moveRight () {
+        if (field.canMove (this.shape, this.x+1, this.y)) {
+            this.erase();
+            this.x=this.x+1;
+            this.draw();
+        }
+    }
+
+    rotateClockwise () {
+
+        let clone = JSON.parse(JSON.stringify(this.shape)); //copying of bidimentional array
+        for (let y = 0; y < clone.length; y++) {
+            for (let x = 0; x < y; x++) {
+                [clone[x][y], clone[y][x]] = [clone[y][x], clone[x][y]];
+            }
+        }
+        clone.forEach(row => row.reverse());
+        for (let i=0;i<5;i++) {
+            let dx = GAMEDATA.kickData[this.rotation][i][0];
+            let dy = GAMEDATA.kickData[this.rotation][i][1];
+            if (this.piece===0) {
+                dx = GAMEDATA.kickDataI[this.rotation][i][0];
+                dy = GAMEDATA.kickDataI[this.rotation][i][1];
+            }
+            if (field.canMove(clone, this.x+dx, this.y+dy)) {
+                this.erase();
+                this.shape = clone.slice();
+                this.x+=dx;
+                this.y+=dy;
+                this.draw();
+                this.rotation++;
+                if (this.rotation === 4) this.rotation = 0;
+                break;
+            }
+        }
+    }
+
+    rotateCounterClockwise () {
+        let clone = JSON.parse(JSON.stringify(this.shape));
+        clone.forEach(row => row.reverse());
+        for (let y = 0; y < clone.length; y++) {
+            for (let x = 0; x < y; x++) {
+                [clone[x][y], clone[y][x]] = [clone[y][x], clone[x][y]];
+            }
+        }
+        for (let i=0;i<5;i++) {
+            let dx = GAMEDATA.kickData[this.rotation+4][i][0];
+            let dy = GAMEDATA.kickData[this.rotation+4][i][1];
+            if (this.piece===0) {
+                dx = GAMEDATA.kickDataI[this.rotation+4][i][0];
+                dy = GAMEDATA.kickDataI[this.rotation+4][i][1];
+            }
+            if (field.canMove(clone, this.x+dx, this.y+dy)) {
+                this.erase();
+                this.shape = clone.slice();
+                this.x+=dx;
+                this.y+=dy;
+                this.draw();
+                this.rotation--;
+                if (this.rotation === -1) this.rotation = 3;
+                break;
+            }
+        }
+    }
 }
 
 window.addEventListener("keydown", function(event) {
+    if (event.defaultPrevented) {
+        return; // Do nothing if event already handled
+    }
     switch(event.code) {
         case "KeyW":
         case "ArrowUp":
-            field.rotateClockwise ();
+            field.tetromino.rotateClockwise ();
             break;
         case "KeyZ":
         case "ControlLeft":
         case "ControlRight":
-            field.rotateCounterClockwise ();
+            field.tetromino.rotateCounterClockwise ();
             break;
         case "KeyA":
         case "ArrowLeft":
-            field.moveLeft ();
+            field.tetromino.moveLeft ();
             break;
         case "KeyD":
         case "ArrowRight":
-            field.moveRight ();
+            field.tetromino.moveRight ();
             break;
         case "KeyS":
         case "ArrowDown":
-            field.moveDown ();
+            field.tetromino.moveDown ();
             break;
    }
-
+    // Consume the event so it doesn't get handled twice
+    event.preventDefault();
 }, true);
 
 
-/*window.onkeydown = window.onkeyup = function(e){
-    keymap[e.code] = e.type === 'keydown';
-};*/
-
 let field = new Field();
 
-function play() {
+function playTetris() {
     field.startGame();
 
 
